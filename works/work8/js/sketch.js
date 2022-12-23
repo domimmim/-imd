3b88626a58b7f0e3d07af60b689e0c9f8f79e5b0
didohan0 <95852944+domimmim@users.noreply.github.com>
2022-12-23 11:53:24 +0900

    Update sketch.js
    c

--- works/work8/js/sketch.js
+++ works/work8/js/sketch.js
@@ -1,174 +1,125 @@
+let blobs = [];
+let i = 0;
 
-const MAX_NUMBER = 280;
+const Blob = function () {
+    stroke(0, 10);
 
+    let xoff = random(10000); // start at diff. noise positions
+    let yoff = random(10000);
+    const speed = random(0.0005); // perlin
+    const lerpAmt = random(0.025, 0.05); // lerp
+    let sortedView = false;
 
-let vertShader = `
-	precision highp float;
+    let d = 10;
+    let x = noise(xoff) * width;
+    let y = noise(yoff) * height;
+    let sortedX = 0;
+    let sortedY = 0;
 
-	attribute vec3 aPosition;
+    if (random(100) < 5) {
+        d = random(60)
+    } else {
+        d = random(10);
+    }
 
-	void main() {
-		vec4 positionVec4 = vec4(aPosition, 1.0);
-		positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
-		gl_Position = positionVec4;
-	}
-`;
+    let mouseMatch = function () {
+        return dist(mouseX, mouseY, x, y) > (d / 2);
+    }
 
-let fragShader = `
-	precision highp float;
-	
-	uniform float time;
-	uniform float a;
-	uniform float slide;
-	uniform vec2 resolution;
-	uniform vec3 particles[${MAX_NUMBER}];
-    uniform vec3 colors[${MAX_NUMBER}];
+    return {
+        update: function () {
+            xoff += speed;
+            yoff += speed;
+            if (!sortedView) {
+                x = lerp(x, noise(xoff) * width, lerpAmt);
+                y = lerp(y, noise(yoff) * height, lerpAmt);
+            } else {
+                x = lerp(x, sortedX, lerpAmt);
+                y = lerp(y, sortedY, lerpAmt);
+            }
+        },
+        display: function () {
+            colorMode(RGB);
+            let colX = map(x, 0, width, 0, 400);
+            let colY = map(y, 0, height, 0, 200);
 
-	
-	void main() {
-			vec2 st = gl_FragCoord.xy / resolution.xy; 
-			
-            // Center coordinates;
-            vec2 st_c = 1.0 - 2.0 * st;
-			
-            // Restore aspect ratio;
-			st_c.y *= resolution.y/resolution.x;
+            if (mouseMatch()) {
+                fill(colX, colY, 255, 200);
+            }
+            else {
+                fill(255, 200);
+            }
 
+            ellipse(x, y, d);
+        },
+        sort: function (pos) {
+            sortedView = !sortedView;
+            sortedX = pos.x;
+            sortedY = pos.y;
+        },
+        mouseMatch, d
+    }
+}
 
-			float angle = a * 2.0 * 5.1415;
-			mat2 R = mat2(cos(angle), sin(angle), -sin(angle), cos(angle));
-			st_c *= R;
+function gradient(
+    col1 = [240, 65, 30],
+    col2 = [340, 65, 50],
+    resolution = 64
+) {
+    colorMode(HSL);
+    for (let y = 0; y < height; y += height / resolution) {
+        noStroke();
 
-			float mult = 0.01;
-			
-      vec3 col = vec3(0.0);
-        
-			for(int i = 0; i < ${MAX_NUMBER}; i++){
-			
-					vec3 particle = particles[i];
-					vec2 pos = particle.xy;
-					float m = particle.z * 0.8;
-					vec3 color = colors[i]/ 255.0;
-					col += color / distance(st_c, pos)  * m * mult ;
-			}
-           
-			vec3 output_color = col;
-			vec3 contr = output_color * output_color;
-			output_color = mix(output_color, contr, slide);
-			gl_FragColor = vec4(output_color, 1.0);
-			
-	}
-`;
+        let lerpAmt = map(y, 0, height, 0, 1);
+        let h = lerp(col1[0], col2[0], lerpAmt);
+        let s = lerp(col1[1], col2[1], lerpAmt);
+        let l = lerp(col1[2], col2[2], lerpAmt);
 
-let shader;
-let buffer, bufferB;
-let palette = ["#FFAD21", "#00DD96", "#A23251", "#C946FF", "#005CAA"];
-let particles = [];
-let colors = [];
-let angle = 1.0;
-let target_angle = 0.5;
-
-
-let time = 0.0;
-let speed = 1.009;
-let w;
-
-function preload() {
-	shader = new p5.Shader(this.renderer, vertShader, fragShader);
+        fill(h,s,l);
+        rect(0, y, width, 1+height/resolution);
+    }
 }
 
 function setup() {
-	let boundingRects = document
+  let boundingRects = document
     .getElementById("p5Canvas")
     .getBoundingClientRect();
   let canvas = createCanvas(boundingRects.width, boundingRects.height);
   canvas.parent("p5Canvas");
-	background(0);
-	w = min(800, 800);
-  pixelDensity(1);
-	
-	buffer = createGraphics(w, w, WEBGL);
-	bufferB = createGraphics(w, w);
-	buffer.noStroke();
 
-	for (let i = 0; i < MAX_NUMBER; i++){
-		let p = [random(-0.2, 0.2), random(-0.2, 0.4), random(0.04, 0.3)];
-		particles.push(p);
-
-		let col = palette[int(random(0, 5))];
-		colors.push([red(col), green(col), blue(col)]);
+	for (each of Array(300)) {
+		blobs.push(Blob());
 	}
-
-	
 }
 
 function draw() {
-	
-	if ((random(0.0, 1.0) < 0.01) & (abs(angle - target_angle) < 0.01)){
-		target_angle = random(0.0, 1.0);
-	  }
-	  
-	angle += (target_angle - angle) * 0.001;
-	
-	let data = prepareData(particles, colors);
+	background(50);
+	gradient();
 
-	shader.setUniform("a", angle);
-	shader.setUniform("slide", 0.9);
-	shader.setUniform("resolution", [w, w]);
-	shader.setUniform("particles", data.particles);
-	shader.setUniform("colors", data.colors);
+	for (const blob of blobs) {
+		blob.update();
+		blob.display();
+	}
+}
 
-	buffer.shader(shader);
-	buffer.rect(0, 0, w, w);
-	
-	bufferB.image(buffer, 0, 0, w, w);
-	
-	
-	
-	image(bufferB, (width - w) * 0.5, (height - w) * 0.5, w, w);
+function mousePressed() {
+	sortBySize();
+}
 
-	// Motion effect;
+function sortBySize() {
+	const sizes = blobs.map(b => b.d);
+	const sorted = [...sizes].sort((a, b) => a - b);
+	const sortedIndexes = sorted.map(el => sizes.indexOf(el));
 
-	for (let i  = 0; i < particles.length; i++){
-		let sp = speed * (1.0 + 0.02 * particles[i][2]);
-		let p = [particles[i][0] * sp, particles[i][1] * sp, particles[i][2] * sp];
-		particles[i] = p;
+	const xy = [];
+
+	for (let y = 0; y < height; y += width / sqrt(blobs.length)) {
+		for (let x = 20; x < width; x += height / sqrt(blobs.length)) {
+			xy.push({ x, y });
+		}
 	}
 
-	// Check boundaries;
-
-	particles.forEach((p, i) => {
-
-		let x = p[0];
-		let y = p[1];
-
-		if (x > 1.0 | x < - 1.0 | y > 1.0 | y < -1.0){
-			p[2] *= 0.8;
-		}
-		if (x > 2.0 | x < - 2.0 | y > 2.0 | y < -2.0){
-			p[0] = random(-0.5, 0.3);
-			p[1] = random(-0.3, 0.3);
-			p[2] = random(0.01, 0.4);
-			let col = palette[int(random(0, 5))];
-			colors[i] = [red(col), green(col), blue(col)];
-		}
-	});
-
-	time += 0.05;
+	for (const [i, b] of sortedIndexes.entries()) {
+		blobs[b].sort(xy[i]);
+	}
 }
-
-
-
-function prepareData(arr, arr2){
-	
-	let data = {"particles": [], "colors" : []}
-	arr.forEach((p, i) => {
-		data["particles"].push(...p);
-		data["colors"].push(...arr2[i]);
-	});
-	return data;
-}
-
-
-
-
